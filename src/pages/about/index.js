ec0b7adf9884b2b98afe94383e9defb6d98a85ab
Dan Chavez <danjokchavez660@gmail.com>
2023-03-18 21:23:48 +0800

    comit for contact and about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import logo from './logo.png';
 
 export default function About() {
-    return (
-        <section className='hero'>
-            <h1>Welcome to About Page</h1>
-        </section>
-    )
+  return (
+    <section className='hero'>
+      <img src={logo} alt='Logo' style={{ height: '271px', width: '399px', borderRadius: '0px', marginBottom: '20px' }} />
+      <h1 style={{ textAlign: 'center' }}>About Us</h1>
+      <p>
+        InfoTech University is an all Information Technology Course University in the Philippines. It was recently established, 
+        on February 6, 2023, with the goal of creating a university that primarily focuses on technology, specifically information technology. It was established with the goal of fostering excellence in students and producing people of note on a national scale.
+      </p>
+      <hr />
+      <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px 0' }}>
+        <div><strong>Facebook Page:</strong> Infotect University</div>
+        <div><strong>Email:</strong> infotechuniversity@infouniv.com</div>
+        <div><strong>Contact Number:</strong> 09090114021</div>
+      </div>
+    </section>
+  )
 }
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from './logo.png';
 
 export default function Contact() {
-    return (
-        <section className='hero'>
-            <h1>Welcome to Contact Page</h1>
-        </section>
-    )
+  return (
+    <section className='hero'>
+      <img src={logo} alt='Logo' style={{ height: '271px', width: '399px', borderRadius: '0px', marginBottom: '20px' }} />
+      <h1 style={{ textAlign: 'center' }}>Contact Us Directly</h1>
+      <hr />
+      <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px 0' }}>
+        <div><strong>Facebook Page:</strong> Infotect University</div>
+        <div><strong>Email:</strong> infotechuniversity@infouniv.com</div>
+        <div><strong>Contact Number:</strong> 09090114021</div>
+      </div>
+    </section>
+  )
 }
