0cd167763e0a4ff1bafe18ca5fc1564acf3dc44d
iamqueenariii <maryrambonanza@gmail.com>
2023-03-22 14:23:17 +0800

    about page edit and logo changed

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import logo from './logo.png';
+import logo from './logoUni.png';
 
 export default function About() {
   return (
-    <section className='hero'>
-      <img src={logo} alt='Logo' style={{ height: '271px', width: '399px', borderRadius: '0px', marginBottom: '20px' }} />
-      <h1 style={{ textAlign: 'center' }}>About Us</h1>
+    <div className='hero'>
+      <div style={{backgroundColor: 'white', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+        <img src={logo} style={{height: 500, width: 'auto', marginLeft: 470, marginTop: -270}}/>
+        <div style={{color: 'black', fontSize: '70px', fontWeight: 'bold', marginTop: '160px', marginLeft:"-27rem"}}><h1>About Us</h1></div>
+      </div>
+
       <p>
+        <div style={{marginTop: '10px'}}>
         InfoTech University is an all Information Technology Course University in the Philippines. It was recently established, 
         on February 6, 2023, with the goal of creating a university that primarily focuses on technology, specifically information technology. It was established with the goal of fostering excellence in students and producing people of note on a national scale.
+        </div>
       </p>
-      <hr />
-      <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px 0' }}>
-        <div><strong>Facebook Page:</strong> Infotect University</div>
-        <div><strong>Email:</strong> infotechuniversity@infouniv.com</div>
-        <div><strong>Contact Number:</strong> 09090114021</div>
-      </div>
-    </section>
+    </div>
   )
 }
